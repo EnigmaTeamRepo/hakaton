2032138cacbad1b7edd5916af9a7547e4e82b99c
Aleksei Kobiakin <akobyakin@gmail.com>
2022-11-20 11:00:14 +0300

    + front

--- src/main/java/com/enigma/hakaton/config/SecurityConfig.java
+++ src/main/java/com/enigma/hakaton/config/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/csrf", "/login", "/logout", "/js/**", "/error**").permitAll()
+                .antMatchers("/", "/csrf", "/login", "/logout", "/js/**", "/css/**", "/error**").permitAll()
                 .anyRequest().authenticated()
                 .and().formLogin().loginProcessingUrl("/login")
                 .and().logout().logoutSuccessUrl("/").permitAll()
---
+++ src/main/resources/static/js/377.f42c3be6.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkclient"]=self["webpackChunkclient"]||[]).push([[377],{9377:function(t,n,e){e.r(n),e.d(n,{default:function(){return o}});var u=e(3396);const a={class:"layout"};function c(t,n,e,c,s,r){return(0,u.wg)(),(0,u.iD)("section",a,[(0,u.WI)(t.$slots,"default")])}var s={name:"DefaultLayout"},r=e(89);const l=(0,r.Z)(s,[["render",c]]);var o=l}}]);
+//# sourceMappingURL=377.f42c3be6.js.map
